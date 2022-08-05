96de1778be0fd284350a67127a0fa93f8748c3f1
Antoni <antonstrasz@gmail.com>
2022-08-05 18:38:56 +0200

    Bot v1.0

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+from twilio.rest import Client
+
+sid="" #add your Account SID here
+authToken="" #add your Auth Token here
+client=Client(sid,authToken)
+
 
 newest = ""
 with open("previous.txt", encoding='utf8') as file:
     newest = file.read().strip()
 
 while True:
-    html = requests.get('https://www.otodom.pl/pl/oferty/wynajem/mieszkanie/wroclaw?distanceRadius=0&page=1&limit=36&market=ALL&roomsNumber=%5BTHREE%5D&priceMin=1800&priceMax=3200&locations=%5Bcities_6-39%5D&viewType=listing').text
+    #this is just an example search, you can change it like you want
+    html = requests.get('https://www.otodom.pl/pl/oferty/wynajem/mieszkanie/wroclaw').text
     soup = BeautifulSoup(html, 'lxml')
     listings = soup.find_all('ul', class_='css-14cy79a e3x1uf06')
     listings = listings[1].find_all('li', class_='css-p74l73 es62z2j19')
-    first = True
-    live_new = newest
-    for i in listings:
-        name = i.div.h3.text
-        if name == newest:
-            break
-        if first:
-            live_new = name
-            first = False
-        print(name)
-        print('https://www.otodom.pl'+i.a['href'])
-    newest = live_new
-    with open('previous.txt', "w" , encoding='utf8') as file:
-        file.write(newest)
+
+    name = listings[0].div.h3.text
+    if name!=newest:
+        url = "https://www.otodom.pl" + listings[0].a['href']
+        newest = name
+        message = client.messages.create(
+            to="whatsapp: +xxxxxxxxxxx", #add your number
+            from_="whatsapp:+xxxxxxxxxxx", #add twilio number
+            body=f"Nazwa: {name} \n" #example body template
+                 f"Url: {url}")
+
+        with open('previous.txt', "w" , encoding='utf8') as file:
+            file.write(newest)
     print("Czekam minutę")
-    time.sleep(10)
+    time.sleep(60)
